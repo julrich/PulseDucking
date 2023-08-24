7afff4f006cd831fd55748c9a3162f5fa80ca9e4
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2023-08-24 09:45:20 +0200

    feature: add ramp up, work off existing volumes

--- react.py
+++ react.py
@@ -2,27 +2,65 @@ import math
 import json
 import subprocess
 import inputs
+import time
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
+fullVolume = 65536
+
 duckPercent = int(config["preferences"]["duckByPercent"]) or 50
-# TODO read current volume value from sink to duck based on that
-FULL_VOLUME = math.floor(65536 * 0.8)
+duckRamp = int(config["preferences"]["rampUpMs"])
+
+volumesOriginal = dict()
+
+
+def current_milli_time():
+    return int(time.time() * 1000)
 
 
 def onPlayStateChange(playing):
     inputsList = inputs.get()
+
     namesToDuck = [a["name"] for a in config["applications"] if a["role"] == "slave"]
     inputsToDuck = [i for i in inputsList if i.name in namesToDuck]
 
-    volume = (
-        math.floor(FULL_VOLUME * ((100 - duckPercent) / 100))
-        if playing
-        else FULL_VOLUME
-    )
-    for input in inputsToDuck:
-        # TODO don't set immediately (jarring), but ramp to target volume
-        r = subprocess.check_output(
-            ["pacmd", "set-sink-input-volume", str(input.index), str(volume)]
-        )
+    starttime = current_milli_time()
+    elapsedtime = 0
+
+    while elapsedtime < duckRamp:
+        elapsedtime = current_milli_time() - starttime
+        time.sleep(0.01)
+        for input in inputsToDuck:
+            print(playing, input)
+            if playing:
+                volumesOriginal[input.index] = int(input.volume)
+                duckedVolumeDifference = math.floor(
+                    volumesOriginal[input.index]
+                    - ((100 - duckPercent) / 100 * volumesOriginal[input.index])
+                )
+                volume = min(
+                    math.floor(
+                        volumesOriginal[input.index]
+                        - (duckedVolumeDifference * (elapsedtime / duckRamp))
+                    ),
+                    fullVolume,
+                )
+            else:
+                volumeStart = int(input.volume)
+                if input.index in volumesOriginal:
+                    volume = min(
+                        math.floor(
+                            volumesOriginal[input.index]
+                            - (
+                                (volumesOriginal[input.index] - volumeStart)
+                                * (1 - (elapsedtime / duckRamp))
+                            )
+                        ),
+                        fullVolume,
+                    )
+                else:
+                    volume = int(input.volume)
+            r = subprocess.check_output(
+                ["pacmd", "set-sink-input-volume", str(input.index), str(volume)]
+            )
